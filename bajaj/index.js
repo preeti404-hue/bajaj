c02f2999b2f952eeee2cfdd11fc2e8d9bcb68012
preeti404-hue <preeti404singh404@gmail.com>
2026-02-10 11:33:12 +0530

    Update index.js

--- bajaj/index.js
+++ bajaj/index.js
@@ -104,7 +104,9 @@ app.get("/health", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log("Server running on port " + PORT);
+
+// });
